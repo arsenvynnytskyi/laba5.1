cbda97766e625c1cd26264d57c6e237c70257375
arsenvynnytskyi <arsen.vynnytskyi.ri.2025@lpnu.ua>
2025-10-08 21:25:25 +0300

    laba5.1

---
+++ 5.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double k(double x, double y);
+
+int main()
+{
+	double p, q;
+
+	cout << "p = "; cin >> p;
+	cout << "q = "; cin >> q;
+	
+	double c =  k(pow(p + sqrt(p), 2), - k(1, p + q));
+	cout << "c = " << c << endl;
+
+	return 0;
+
+}
+double k(const double x,const double y)
+{
+	
+	return x / abs((pow(x, 3) + pow(y, 3) + y / (x - y)));
+}
