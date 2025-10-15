108aba5a97b1c1ab97d480236b659575c45bbf9c
arsenvynnytskyi <arsen.vynnytskyi.ri.2025@lpnu.ua>
2025-10-15 08:40:49 +0300

    test5.1

--- 5.1.cpp
+++ 5.1.cpp
@@ -8,18 +8,21 @@ double k(double x, double y);
 int main()
 {
 	double p, q;
+    
+    cout << "p = "; cin >> p;
+    cout << "q = "; cin >> q;
 
-	cout << "p = "; cin >> p;
-	cout << "q = "; cin >> q;
-	
-	double c =  k(pow(p + sqrt(p), 2), - k(1, p + q));
-	cout << "c = " << c << endl;
+    double k1 = k(p + sqrt(q), q - sqrt(p));
+    double k2 = k(1, p + q);
+
+    double c = pow(k1, 2) - k2;
+
+    cout << "c = " << c << endl;
 
 	return 0;
 
 }
 double k(const double x,const double y)
 {
-	
-	return x / abs((pow(x, 3) + pow(y, 3) + y / (x - y)));
+    return x / abs(pow(x, 3) + pow(y, 3)) + y / abs(x - y);
 }
